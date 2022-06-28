ad8426ad9533f95d4b071e6625e50c31ac111fa3
Robert Jones <dynasty_warrior201990@yahoo.com>
2022-06-28 03:06:32 -0500

    Added score and result option.

--- javascript.js
+++ javascript.js
@@ -1,44 +1,45 @@
-//make a program to play rock paper scissors vs computer.
-            //code needs to be in javascript. 
+        //make a program to play rock paper scissors vs computer.
+        //code needs to be in javascript. 
 
-            //declaring a way to keep score.   
-            let userscore = 0;
-            let computerscore = 0;
-            let tiegames = 0;
-            let playerSelection;
-            const playerPick = document.querySelectorAll(".userChoice");
-           
-                    // play 5 rounds, using a loop.
+        //declaring a way to keep score.   
+    let userscore = 0;
+    let computerscore = 0;
+    let tiegames = 0;
+    let playerSelection;
+    const playerPick = document.querySelectorAll(".userChoice");
+    const gameWinner = document.querySelector("#gameWinner");
+     
+
         
         
-                //for (let i = 0; i < 5; i++) {
+        //for (let i = 0; i < 5; i++) {
         
         
-                             //name the the variable for what the computer picks, name of varible computerPlay.
-                             //ComputerPlay needs to pick Rock, Paper, Scissors randomly for its Choice and return it.
+
  
         
-                              //use prompt to get anwser for player choice.
+                              //use buttons for player to select.
 //function game (){
      
 
-                        playerPick.forEach(button => button.addEventListener("click", () => {
-                            playerSelection = button.textContent;
-                            computerPlay();
-                            const computerSelection = computerPlay();
-                             //make console.log to see value in console
-                            console.log ('The player chose: ' + playerSelection);
-                            console.log ("The computer chose: " + computerSelection);       
-                            playRound(computerSelection,playerSelection);
-                            console.log (gameresult());
-                        }));
 
-                    
-                    function computerPlay(){
-                        let computerselections = ['Rock', 'Paper', 'Scissors'];
-                        let computerselection = computerselections[Math.floor(Math.random()*computerselections.length)];
-                        return computerselection;
-                    }  
+                playerPick.forEach(button => button.addEventListener("click", () => {
+                    playerSelection = button.textContent;
+                    computerPlay();
+                    const computerSelection = computerPlay();
+                        //make console.log to see value in console     
+                    console.log(playRound(playerSelection, computerSelection))
+                    gameresult(userscore,computerscore);
+                    gameWinner.textContent = gameresult(userscore, computerscore);
+                }));
+
+                        //name the the variable for what the computer picks, name of varible computerPlay.
+                        //ComputerPlay needs to pick Rock, Paper, Scissors randomly for its Choice and return it.
+                function computerPlay(){
+                    let computerselections = ['Rock', 'Paper', 'Scissors'];
+                    let computerselection = computerselections[Math.floor(Math.random()*computerselections.length)];
+                    return computerselection;
+                }  
                                        
                   
                 
@@ -47,59 +48,58 @@
                         //write a function to play a round of the game
                         //the function needs two parameters, playerselection and computerselection.
                         // need to return the winner of the round. will be used later.
-                        function playRound(playerSelection, computerSelection){
-        
-                        if(playerSelection == 'Scissors'){
-                            if (computerSelection =='Paper'){
+                function playRound(playerSelection, computerSelection){
+                    console.log ('The player chose: ' + playerSelection);
+                    console.log ("The computer chose: " + computerSelection);
+                           
+
+                        if(playerSelection  === computerSelection){
+                            tiegames++;
+                            return ('Tie Game');
+                        }
+                       else if(playerSelection === 'Scissors'){
+                            if (computerSelection === 'Paper'){
                                 userscore++;
                                 return ('You Won!!! Scissors beat Paper');
                             }
-                            else if (computerSelection == 'Rock'){
+                            else if (computerSelection === 'Rock'){
                                 computerscore++;
                                 return ('Computer Won! Rock beat Scissors');
-                            }
-                            else {
-                                tiegames++;
-                                return ('Tie');
                             }
                         } 
                         
                        
-                        else if (playerSelection == 'Paper') {
-                            if (computerSelection == 'Rock'){
+                        else if (playerSelection === 'Paper') {
+                            if (computerSelection === 'Rock'){
                                 userscore++;
                                 return ('You Won!!! Paper beats Rock');
                             }
-                            else if (computerSelection ==='Scissors'){
+                            else if (computerSelection === 'Scissors'){
                                 computerscore++;
                                 return ('Computer Won! Scissors beat Paper');
                             }
-                            else {
-                                tiegames++;
-                                return ('Tie Game');
+
                             }    
-                        }
-                        else if (playerSelection == 'Rock') {
-                            if (computerSelection == 'Scissors'){
+                        
+                        else if(playerSelection === 'Rock') {
+                            if (computerSelection === 'Scissors'){
                                 userscore++;
                                 return ('You Won!!! Rock beats Scissor' );
                             }
-                            else if (computerSelection == 'Paper'){
+                            else if (computerSelection === 'Paper'){
                                 computerscore++;
                                 return ('Computer Won! Paper beats Rock');
                             }
-                            else {
-                                tiegames++;
-                                return ('Tie Game');
-                            }
                         }
-                        }
-                    //console.log(playRound(playerSelection, computerSelection));
+                     
+                    }
+              
                     //}
-        
-                    console.log (userscore, computerscore, tiegames);
-        
+   
+                   
                     function gameresult(userscore, computerscore){
+                  
+                                        
                         if (userscore > computerscore) {
                             return ('Congratz, You beat a Computer!');
                         }
@@ -109,8 +109,11 @@
                         else {
                             return ('Tie Score');
                         }
-                       
+                        
+                
+                    
                     }
-        
-                alert(gameresult(userscore, computerscore));
-               // }
+ 
+                
+
+            
